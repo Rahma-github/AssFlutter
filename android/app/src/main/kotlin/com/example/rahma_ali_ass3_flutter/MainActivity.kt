9579c5b8cec22b92be9856b6e3ae1f3c9d737b00
Rahma-github <ralihassan538@gmail.com>
2024-03-18 13:49:12 +0200

    flutter page

---
+++ android/app/src/main/kotlin/com/example/rahma_ali_ass3_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rahma_ali_ass3_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          color: Colors.blue,
+        ),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final double textFieldWidth = MediaQuery.of(context).size.width * 0.7;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Login Screen App',
+          style: TextStyle(
+            color: Colors.white, // Set app bar title text color to white
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 5), // Reduce space between 'codeplayon' and top of page
+            const Text(
+              'codeplayon',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 36.0, // Increase the font size
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0), 
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'User name',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0), 
+                    child: TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextButton(
+                    onPressed: () {
+                      
+                    },
+                    child: Text(
+                      'Forgot Password',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0), 
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded( 
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 10.0), 
+                            child: TextButton(
+                              onPressed: () {
+                                
+                              },
+                              style: TextButton.styleFrom(
+                                padding: EdgeInsets.symmetric(horizontal: 50), 
+                                backgroundColor: Colors.blue,
+                                primary: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(4), 
+                                  side: BorderSide(color: Colors.blue),
+                                ),
+                              ),
+                              child: Text('Login'),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Does not have account? ',
+                        style: TextStyle(
+                          color: Colors.black, 
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          
+                        },
+                        child: Text(
+                          'Sign in',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
